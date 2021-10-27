7ec8c346c75507a1ce491a847d42c02dab7f065b
Myzwer <josh@onefortyfivedesign.com>
2021-10-27 11:43:54 -0400

    :rocket: Finished the header / hero up for now. Might want to redesign it later, but for now it's done.

--- frontpage.php
+++ frontpage.php
@@ -24,131 +24,22 @@ get_header(); ?>
              alt="">
     </div>
 
-    <div class="bg-white pb-10">
-        <div class="m-4 md:m-10 lg:max-w-4xl lg:text-center lg:mx-auto pt-10">
-            <div class="grid">
-                <div class="grid-cols-12">
-                    <div class="col-span-12">
-                        <div class="text-center md:text-left mb-1">
-                            <h1>Color Pod</h1>
-                            <p>All fields optional</p>
-                        </div>
-                        <hr>
-                    </div>
-                </div>
-            </div>
-
-            <div class="grid grid-cols-12 gap-4 mt-6">
-                <div class="col-span-12 lg:col-span-4 card-gradient-1 rounded-xl shadow-xl">
-                    <div class="text-center p-4">
-                        <h2 class="font-bold text-2xl">Title</h2>
-                        <h4 class="font-semibold">Subtitle</h4>
-                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam
-                            commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore
-                            magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
-                        <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                            Call To Action
-                        </button>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-4 card-gradient-2 rounded-xl shadow-xl">
-                    <div class="text-center p-4">
-                        <h2 class="font-bold text-2xl">Title</h2>
-                        <h4 class="font-semibold">Subtitle</h4>
-                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam
-                            commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore
-                            magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
-                        <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                            Call To Action
-                        </button>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-4 card-gradient-3 rounded-xl shadow-xl">
-                    <div class="text-center p-4">
-                        <h2 class="font-bold text-2xl">Title</h2>
-                        <h4 class="font-semibold">Subtitle</h4>
-                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam
-                            commodi consequuntur distinctio doloribus eaque, earum exercitationem, fuga iste labore
-                            magni, maxime molestiae nulla pariatur quod sapiente totam vel voluptate?</p>
-                        <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                            Call To Action
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="grid grid-cols-12 mt-20">
+    <div class="bg-white pb-5 md:pb-0.5">
+        <div class="m-4 md:m-10 lg:max-w-6xl lg:text-center lg:mx-auto pt-10">
+            <div class="grid grid-cols-12">
                 <div class="col-span-12">
-                    <div class="text-center md:text-left mb-1">
-                        <h1>PDF Download (four-up)</h1>
-                        <p>This configuration always shows four PDF's on one row.</p>
-                    </div>
-                    <hr>
-                </div>
-            </div>
-
-
-            <div class="grid grid-cols-12 mt-5 gap-6">
-                <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                    <div class="text-center mb-1">
-                        <div class="w-24 text-center m-auto my-3">
-                            <img class="rounded-full"
-                                 src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
-                                 alt="">
-                        </div>
-                        <h2 class="font-bold text-2xl">PDF Title</h2>
-                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis
-                            cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio
-                            quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                    <div class="text-center mb-1">
-                        <div class="w-24 text-center m-auto my-3">
-                            <img class="rounded-full"
-                                 src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
-                                 alt="">
-                        </div>
-                        <h2 class="font-bold text-2xl">PDF Title</h2>
-                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis
-                            cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio
-                            quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                    <div class="text-center mb-1">
-                        <div class="w-24 text-center m-auto my-3">
-                            <img class="rounded-full"
-                                 src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
-                                 alt="">
-                        </div>
-                        <h2 class="font-bold text-2xl">PDF Title</h2>
-                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis
-                            cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio
-                            quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
-                    </div>
-                </div>
-
-                <div class="col-span-12 md:col-span-6 lg:col-span-3">
-                    <div class="text-center mb-1">
-                        <div class="w-24 text-center m-auto my-3">
-                            <img class="rounded-full"
-                                 src="https://images.unsplash.com/photo-1508515053963-70c7cc39dfb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
-                                 alt="">
-                        </div>
-                        <h2 class="font-bold text-2xl">PDF Title</h2>
-                        <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam corporis
-                            cumque doloremque error esse exercitationem explicabo iure iusto magni molestiae, nulla odio
-                            quam quo repellat vero. Aliquam possimus repellendus voluptate.</p>
+                    <div class="text-left px-2 mb-1">
+                        <h2 class="text-2xl pb-3 md:pr-5 inline-block">Helping pastors create healthy environments that grow healthy
+                            churches.</h2>
+                        <button class="uppercase inline-block rounded-md py-3 px-6 text-white bg-gray-dark hover:bg-gray-darkest transition duration-300">
+                            Take Your First Step
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 
 <?php
 get_footer();
