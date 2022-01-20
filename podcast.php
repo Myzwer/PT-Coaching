e21f100ac0a7d5cecd46d4ca05bf42eb44f30836
Myzwer <josh@onefortyfivedesign.com>
2022-01-20 11:30:53 -0500

    :heavy_plus_sign: Added templates and php files for all the other page templates needed for the site.

---
+++ podcast.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: Podcast
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage PT_Coaching
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+<div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
+  rgba(0, 0, 0, 0.45),
+  rgba(0, 0, 0, 0.45)
+), url('https://images.unsplash.com/photo-1501612780327-45045538702b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80') center center;
+ height: 40vh;">
+    <div class="content-middle text-white text-center">
+        <h1 class="text-4xl md:text-5xl mb-3">Contact</h1>
+    </div>
+</div>
+
+
+
+
+<?php get_footer();
