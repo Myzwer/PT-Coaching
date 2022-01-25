07ce9eb7b421302c0069e5bf41514ed9d65d8bf7
Myzwer <josh@onefortyfivedesign.com>
2022-01-25 16:55:31 -0500

    :construction: Completed the main structure of the page, though tablet is still a little broken and the icons in the connected box need addressed as well.

--- about.php
+++ about.php
@@ -16,13 +16,53 @@ get_header(); ?>
   rgba(0, 0, 0, 0.45)
 ), url('https://images.unsplash.com/photo-1501612780327-45045538702b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80') center center;
  height: 40vh;">
-    <div class="content-left text-white text-left">
+    <div class="md:w-2/3 mx-auto grid grid-cols-12 gap-4 pt-40 text-white">
+        <div class="col-span-12 md:col-span-10 px-3">
         <h1 class="text-4xl md:text-5xl mb-3">Pastor Trent</h1>
         <p class="text-xl md:text-2xl mb-3">My desire is to coach pastors to lead churches that connect more people to the Gospel.</p>
+        </div>
     </div>
 </div>
 
+<div class="bg-gray-light bio pb-20">
+    <div class="md:w-3/4 mx-auto grid grid-cols-12 gap-4 p-2">
+        <div class="col-span-12 md:col-span-12 lg:col-span-8 mt-5 px-3">
+            <h2 class = "text-3xl md:text-4xl mb-1 font-bold">About Pastor Trent</h2>
+            <p>Dr. Trent Stewart is the lead pastor of Foothills Church, a multisite church in Maryville and Knoxville TN. The church is the heart and vision of Pastor Trent who planted FC in 2009 with the support of Grace Baptist Church in Knoxville and Pastor Trent’s father, Dr. Ron Stewart – the longtime lead pastor of GBC. Since 2009, Pastor Trent’s vision and hope for FC derives from a deep passion to develop mature disciples of Christ in relational environments. Through the ministry of Foothills Church, he and the FC staff have seen phenomenal growth and ministry take place in their city and even across the globe.</p>
+            <p>Dr. Trent Stewart is the lead pastor of Foothills Church, a multisite church in Maryville and Knoxville TN. The church is the heart and vision of Pastor Trent who planted FC in 2009 with the support of Grace Baptist Church in Knoxville and Pastor Trent’s father, Dr. Ron Stewart – the longtime lead pastor of GBC. Since 2009, Pastor Trent’s vision and hope for FC derives from a deep passion to develop mature disciples of Christ in relational environments. Through the ministry of Foothills Church, he and the FC staff have seen phenomenal growth and ministry take place in their city and even across the globe.</p>
+        </div>
+    </div>
 
+    <div class="md:w-3/4 mx-auto grid grid-cols-12 gap-4 p-2">
+        <div class="col-span-12 md:col-span-12 lg:col-span-8 px-3">
+            <img class = "shadow-lg rounded-lg" src="https://pastor-trent-coaching.local/wp-content/uploads/2020/04/Trent-Family-Photo-rectangle-1024x687-1.jpg" alt="">
+        </div>
+
+        <div class="col-span-12 lg:col-span-4 px-3">
+
+            <div class="grid grid-cols-12 gap-4">
+                <div class="col-span-12 md:col-span-6 lg:col-span-12">
+                    <div class="bg-gray-lightest shadow-lg rounded-lg p-5">
+                        <h3 class = "text-2xl md:text-3xl mb-1 font-bold">Want Certified Coaching?</h3>
+                        <button class="uppercase inline-block rounded-md mt-3 py-3 px-6 text-white bg-gray-dark hover:bg-gray-darkest transition duration-300">
+                            Take Your First Step
+                        </button>
+                    </div>
+                </div>
+
+                <div class="col-span-12 md:col-span-6 lg:col-span-12">
+                    <div class="bg-gray-lightest shadow-lg rounded-lg p-5">
+                        <h3 class = "text-2xl md:text-3xl mb-1 font-bold">Stay Connected</h3>
+                        <a href="https://www.facebook.com/trentonjstewart"><i class="fa fa-facebook" aria-label="Trent Stewart Facebook"></i></a>
+                        <a href="https://www.instagram.com/trentonjstewart/?hl=en"><i class="fa fa-instagram" aria-label="Trent Stewart Instagram"></i></a>
+                        <a href="https://www.youtube.com/channel/UC2_Vo0eFmYaMq3SiA0PQLaQ"><i class="fa fa-youtube" aria-label="Trent Stewart Youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
 
 
 <?php get_footer();
