197f2ab3f450c3b390d396e1ed65ac845ae485a5
Myzwer <josh@onefortyfivedesign.com>
2022-01-31 11:24:43 -0500

    :art: Made some adjustments to the watch page to fix a visual bug with the latest sermon card. Also rounded the corners.

--- sermon.php
+++ sermon.php
@@ -23,17 +23,17 @@ get_header(); ?>
     <div class="bg-gray bio pb-5">
         <div class="md:w-3/4 mx-auto grid grid-cols-12 p-2">
 
+            <!-- Featured Sermon -->
             <div class="col-span-12 text-center mx-auto mt-10">
                 <h3 class="text2xl md:text-3xl mb-3 font-bold">Latest Sermon</h3>
             </div>
 
-            <div class="col-span-12 md:col-span-4 text-center mx-auto">
-                <img class="shadow-lg"
+            <div class="col-span-12 lg:col-span-6 text-center mx-auto bg-gray-light lg:rounded-l-lg">
+                <img class="shadow-lg rounded-t-lg md:rounded-t-none md:rounded-l-lg"
                      src="https://pastor-trent-coaching.local/wp-content/uploads/2021/10/10-17-21-Sermon.jpg" alt="">
             </div>
-
-            <div class="col-span-12  md:col-span-8 p-5 bg-gray-light shadow-lg">
-                <h3 class="text-xl md:text-2xl mb-1 font-bold">Busy Pastor Podcast</h3>
+            <div class="col-span-12 lg:col-span-6 p-5 bg-gray-light shadow-lg rounded-b-lg md:rounded-b-none md:rounded-r-lg">
+                <h3 class="text-xl md:text-2xl mb-1 font-bold">Why Share? | Gospel Voice</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur autem eius eum ipsam
                     qui quod veritatis vitae. Amet, assumenda distinctio dolore iusto molestias nihil optio quia
                     similique unde voluptates?</p>
@@ -41,12 +41,14 @@ get_header(); ?>
                     Watch Now
                 </button>
             </div>
+            <!-- End Featured Sermon -->
 
+            <!-- All Sermons -->
             <div class="col-span-12 text-center mx-auto mt-10">
                 <h3 class="text2xl md:text-3xl mb-3 font-bold">All Sermons</h3>
             </div>
 
-            <div class="col-span-12 p-5 mb-8 bg-gray-light shadow-lg">
+            <div class="col-span-12 p-5 mb-8 bg-gray-light shadow-lg rounded-lg">
                 <h3 class="text-xl md:text-2xl mb-1 font-bold">Busy Pastor Podcast</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur autem eius eum ipsam
                     qui quod veritatis vitae. Amet, assumenda distinctio dolore iusto molestias nihil optio quia
@@ -56,7 +58,7 @@ get_header(); ?>
                 </button>
             </div>
 
-            <div class="col-span-12 p-5 mb-8 bg-gray-light shadow-lg">
+            <div class="col-span-12 p-5 mb-8 bg-gray-light shadow-lg rounded-lg">
                 <h3 class="text-xl md:text-2xl mb-1 font-bold">Busy Pastor Podcast</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur autem eius eum ipsam
                     qui quod veritatis vitae. Amet, assumenda distinctio dolore iusto molestias nihil optio quia
@@ -65,6 +67,8 @@ get_header(); ?>
                     Watch Now
                 </button>
             </div>
+
+            <!-- End Featured Sermon -->
         </div>
     </div>
 
