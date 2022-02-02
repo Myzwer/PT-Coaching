5eed586ff641dfcb7bfb10bb54b351fbb46eade8
Myzwer <josh@onefortyfivedesign.com>
2022-02-02 15:15:48 -0500

    :sparkle: Converted the coaching page to ACF.

--- coaching.php
+++ coaching.php
@@ -29,21 +29,27 @@ get_header(); ?>
 
     <div class="bg-gray-light text-center mx-auto py-10">
         <div class="col-span-12 my-2 md:my-5 px-3 text-left md:text-center">
-            <h2 class = "text-3xl md:text-4xl mb-1 font-bold">Pastor Trent works for Courage to Lead - Pastor Division</h2>
+            <h2 class = "text-3xl md:text-4xl mb-1 font-bold"><?php the_field('cta_text'); ?></h2>
         </div>
-        <button class="uppercase inline-block rounded-md py-3 px-6 text-white bg-gray-dark hover:bg-gray-darkest transition duration-300">
-            Take Your First Step
-        </button>
+        <?php if (have_rows('cta_button')): ?>
+            <?php while (have_rows('cta_button')): the_row(); ?>
+                <a href="<?php the_sub_field('button_link') ?>">
+                    <button class="uppercase inline-block rounded-md py-3 px-6 text-white bg-gray-dark hover:bg-gray-darkest transition duration-300">
+                        <?php the_sub_field('button_text') ?>
+                    </button>
+                </a>
+            <?php endwhile; ?>
+        <?php endif; ?>
     </div>
 
     <div class="bg-gray bio">
         <div class="md:w-3/4 mx-auto grid grid-cols-12 gap-4 p-5">
             <div class="col-span-12 md:col-span-6 lg:col-span-8 mt-5 px-3 video-wrapper">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/G44xTr8D_bw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <?php the_field('video_invite'); ?>
             </div>
             <div class="col-span-12">
-                <h3 class = "text-xl md:text-2xl mb-1 font-bold">HELPING CHURCHES CREATE HEALTHY ENVIRONMENTS THAT GROW HEALTHY MINISTRIES</h3>
-                <p>Every church is unique and requires a tailored approach. With our detailed coaching process, our aim is to help you create a healthy environment that will lead to a culture of multiplying disciples for God’s Kingdom. We have also partnered with the organization Courage to Lead to provide you an opportunity lead at the next level through leadership coaching. If you're ready to take your next step as a leader, contact us today.</p>
+                <h3 class = "text-xl md:text-2xl mb-1 font-bold"><?php the_field('invite_title'); ?></h3>
+                <p><?php the_field('invite_text'); ?></p>
             </div>
         </div>
     </div>
