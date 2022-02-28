067aef5117eee7c943a0d120ddb138076650851e
Myzwer <josh@onefortyfivedesign.com>
2022-02-28 16:09:19 -0500

    :big: Fixed an issue where buttons in bottom section didn't align due to variable copy.

--- frontpage.php
+++ frontpage.php
@@ -27,14 +27,14 @@ get_header(); ?>
     <div class="bg-white pb-5 md:pb-0.5">
         <div class="md:m-10 lg:max-w-6xl lg:text-center lg:mx-auto pt-10">
             <div class="grid grid-cols-12">
-                <div class="col-span-12">
-                    <div class="text-left px-2 mb-1">
-                        <h2 class="text-2xl pb-3 md:pr-5 inline-block"><?php the_field('primary_cta_text') ?></h2>
+                <div class="col-span-12 text-center mx-auto">
+                    <div class="px-2 mb-1">
+                        <h2 class="text-2xl pb-3 md:pr-5 md:inline-block"><?php the_field('primary_cta_text') ?></h2>
 
                         <?php if (have_rows('primary_cta_button')): ?>
                             <?php while (have_rows('primary_cta_button')): the_row(); ?>
                                 <a href="<?php the_sub_field('button_link') ?>">
-                                    <button class="uppercase inline-block rounded-md py-3 px-6 text-white bg-gray-dark hover:bg-gray-darkest transition duration-300">
+                                    <button class="uppercase md:inline-block rounded-md py-3 px-6 text-white bg-gray-dark hover:bg-gray-darkest transition duration-300">
                                         <?php the_sub_field('button_text') ?>
                                     </button>
                                 </a>
@@ -49,7 +49,7 @@ get_header(); ?>
     <div class="bg-gray py-20">
         <div class="md:w-2/3 mx-auto grid grid-cols-12 gap-4 p-4">
             <div class="col-span-12 md:col-span-4">
-                <img src="<?php the_field('section_1_graphic') ?>" alt="">
+                <img class = "rounded-lg shadow-lg" src="<?php the_field('section_1_graphic') ?>" alt="">
             </div>
 
             <div class="col-span-12 md:col-start-6 md:col-span-7 lg:pt-12">
@@ -78,7 +78,7 @@ get_header(); ?>
                     <?php while (have_rows('slot_1')): the_row(); ?>
                         <div class="col-span-12 md:col-span-4 text-center bg-gray-middle py-14 px-2">
                             <h3 class="text-xl"><?php the_sub_field('title') ?></h3>
-                            <p><?php the_sub_field('body_text') ?></p>
+                            <p class = "lg:h-14"><?php the_sub_field('body_text') ?></p>
 
                             <a href="<?php the_sub_field('button_link') ?>">
                                 <button class="rounded-md py-3 px-6 mt-4 text-black bg-gray hover:bg-white-true transition duration-300 drop-shadow-lg">
@@ -94,7 +94,7 @@ get_header(); ?>
                     <?php while (have_rows('slot_2')): the_row(); ?>
                         <div class="col-span-12 md:col-span-4 text-center bg-gray-light py-14 px-2">
                             <h3 class="text-xl"><?php the_sub_field('title') ?></h3>
-                            <p><?php the_sub_field('body_text') ?></p>
+                            <p class = "lg:h-14"><?php the_sub_field('body_text') ?></p>
 
                             <a href="<?php the_sub_field('button_link') ?>">
                                 <button class="rounded-md py-3 px-6 mt-4 text-black bg-gray hover:bg-white-true transition duration-300 drop-shadow-lg">
@@ -110,7 +110,7 @@ get_header(); ?>
                     <?php while (have_rows('slot_3')): the_row(); ?>
                         <div class="col-span-12 md:col-span-4 text-center bg-gray-lightest py-14 px-2">
                             <h3 class="text-xl"><?php the_sub_field('title') ?></h3>
-                            <p><?php the_sub_field('body_text') ?></p>
+                            <p class = "lg:h-14"><?php the_sub_field('body_text') ?></p>
 
                             <a href="<?php the_sub_field('button_link') ?>">
                                 <button class="rounded-md py-3 px-6 mt-4 text-black bg-gray hover:bg-white-true transition duration-300 drop-shadow-lg">
