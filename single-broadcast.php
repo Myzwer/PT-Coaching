228c325030609cd1883d133ce6ce3a0d58201160
Myzwer <josh@onefortyfivedesign.com>
2022-03-23 16:05:48 -0400

    :art: Cleaned up the sermon subpages a bit. Fixed some centering, made the width wider and rounded the corners of the box.

--- single-broadcast.php
+++ single-broadcast.php
@@ -8,14 +8,14 @@ get_header();
 ?>
 
     <div class="bg-gray pb-5 pt-12">
-        <div class="md:w-3/4 mx-auto grid grid-cols-12 p-2">
+        <div class="md:w-8/12 mx-auto grid grid-cols-12 p-2">
 
-            <div class="col-span-12 lg:col-span-10">
+            <div class="col-span-12 ">
                 <div class="relative video-wrapper">
                     <?php the_field('video_link'); ?>
                 </div>
             </div>
-            <div class="col-span-12 lg:col-span-10 p-5 bg-gray-light shadow-lg ">
+            <div class="col-span-12 p-5 bg-gray-light shadow-lg rounded-b-xl">
                 <h3 class="text-xl md:text-2xl mb-1 font-bold"><?php echo the_title(); ?>
                     </h3>
                 <p><?php the_date(); ?></p>
@@ -46,11 +46,11 @@ foreach ($terms as $term) {
 ?>
 
     <div class="bg-gray pb-5">
-        <div class="md:w-3/4 mx-auto grid grid-cols-12 p-2">
+        <div class="md:w-8/12 mx-auto grid grid-cols-12 p-2">
 
-            <div class="col-span-12 lg:col-span-10 p-5 bg-gray-light text-center shadow-lg ">
+            <div class="col-span-12 p-5 bg-gray-light text-center shadow-lg rounded-xl">
                 <a href="">
-                    <?php next_post_link('%link', '<button class="uppercase inline-block rounded-md mt-3 py-3 px-6 text-white bg-gray-dark hover:bg-gray-darkest transition duration-300">Next' . $formatNameSingle . '</button>', true, "", 'format'); ?>
+                    <?php next_post_link('%link', '<button class="uppercase inline-block rounded-md my-3 py-3 px-6 text-white bg-gray-dark hover:bg-gray-darkest transition duration-300">Next' . $formatNameSingle . '</button>', true, "", 'format'); ?>
                 </a>
 
                 <a href="/<?php
