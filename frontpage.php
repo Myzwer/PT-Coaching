b9683e4134a4063a331e9c1aeb9c21c11f512a8a
Myzwer <josh@onefortyfivedesign.com>
2022-03-23 11:01:17 -0400

    :art: Fixed a variety of little padding and spacing things on the frontpage. No major content edits, just spacing.

--- frontpage.php
+++ frontpage.php
@@ -18,8 +18,7 @@ get_header(); ?>
            playsinline muted></video>
 
     <div class="video-overlay">
-        <h1 class="text-white m-auto text-center tracking-wider text-4xl md:text-6xl"><?php the_field('display_title') ?></h1>
-        <img class="m-auto text-center -mt-20 md:-mt-32 w-36 md:w-56"
+        <img class="m-auto text-center -mt-20 md:-mt-18 w-56 md:w-60"
              src="<?php the_field('knockout_logo') ?>"
              alt="">
     </div>
@@ -54,12 +53,12 @@ get_header(); ?>
 
             <div class="col-span-12 md:col-start-6 md:col-span-7 lg:pt-12">
                 <h3 class="text-2xl font-bold"><?php the_field('section_1_title') ?></h3>
-                <p class="py-5"><?php the_field('section_1_paragraph') ?></p>
+                <p class="pb-5"><?php the_field('section_1_paragraph') ?></p>
 
                 <?php if (have_rows('section_1_cta_button')): ?>
                     <?php while (have_rows('section_1_cta_button')): the_row(); ?>
                         <a href="<?php the_sub_field('button_link') ?>">
-                            <button class="uppercase inline-block rounded-md py-3 px-6 text-white bg-gray-dark hover:bg-gray-darkest transition duration-300">
+                            <button class="uppercase inline-block rounded-md py-3 px-6 text-black border-2 border-black hover:drop-shadow-lg transition duration-300">
                                 <?php the_sub_field('button_text') ?>
                             </button>
                         </a>
@@ -76,7 +75,7 @@ get_header(); ?>
 
                 <?php if (have_rows('slot_1')): ?>
                     <?php while (have_rows('slot_1')): the_row(); ?>
-                        <div class="col-span-12 md:col-span-4 text-center bg-gray-middle py-14 px-2">
+                        <div class="col-span-12 md:col-span-4 text-center bg-gray-middle py-14 px-4">
                             <h3 class="text-xl"><?php the_sub_field('title') ?></h3>
                             <p class = "lg:h-14"><?php the_sub_field('body_text') ?></p>
 
@@ -92,7 +91,7 @@ get_header(); ?>
 
                 <?php if (have_rows('slot_2')): ?>
                     <?php while (have_rows('slot_2')): the_row(); ?>
-                        <div class="col-span-12 md:col-span-4 text-center bg-gray-light py-14 px-2">
+                        <div class="col-span-12 md:col-span-4 text-center bg-gray-light py-14 px-4">
                             <h3 class="text-xl"><?php the_sub_field('title') ?></h3>
                             <p class = "lg:h-14"><?php the_sub_field('body_text') ?></p>
 
@@ -108,7 +107,7 @@ get_header(); ?>
 
                 <?php if (have_rows('slot_3')): ?>
                     <?php while (have_rows('slot_3')): the_row(); ?>
-                        <div class="col-span-12 md:col-span-4 text-center bg-gray-lightest py-14 px-2">
+                        <div class="col-span-12 md:col-span-4 text-center bg-gray-lightest py-14 px-4">
                             <h3 class="text-xl"><?php the_sub_field('title') ?></h3>
                             <p class = "lg:h-14"><?php the_sub_field('body_text') ?></p>
 
