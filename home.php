93394f357e70f2fec560b846f35a846d2e18790e
Myzwer <josh@onefortyfivedesign.com>
2022-03-29 16:13:51 -0400

    :green_heart: Updated comments so I can tell what is going on in this mess of PHP

--- home.php
+++ home.php
@@ -38,24 +38,31 @@ if (is_home()) {
 <?php endif; ?>
 
 
-    <!-- Featured Post -->
+    <!-- Start Background -->
     <div class="bg-gray pb-5">
-        <div class="md:w-3/4 xl:w-7/12 mx-auto grid grid-cols-12 p-5">
 
-            <?php $posts_query = new WP_Query('posts_per_page=1'); //limit post to 1 since this is our featured post
+        <!-- Featured Post -->
+        <div class="md:w-3/4 xl:w-7/12 mx-auto grid grid-cols-12 p-5">
+            <!-- Start the loop -->
+            <?php
+            $posts_query = new WP_Query('posts_per_page=1'); // limit post to 1 since this is our featured post
             while ($posts_query->have_posts()) :
             $posts_query->the_post();
             ?>
-            <!-- ******** THE FEATURED SERMON ******** -->
+
             <!-- "Latest sermon" text -->
             <div class="col-span-12 text-center mx-auto">
                 <h3 class="text-2xl md:text-3xl mb-3 font-bold"><?php the_field('body_title_1', $post_id); ?></h3>
             </div>
+            <!-- End "Latest sermon" text -->
 
+            <!-- Get Post Thumbnail -->
             <div class="col-span-12 lg:col-span-7">
                 <?php the_post_thumbnail('post-thumbnail', array('class' => 'shadow-lg rounded-t-lg md:rounded-t-none md:rounded-l-lg')); ?>
             </div>
+            <!-- End Post Thumbnail -->
 
+            <!-- Info / Link Side of card -->
             <div class="col-span-12 lg:col-span-5 p-5 bg-gray-light shadow-lg rounded-b-lg md:rounded-b-none md:rounded-r-lg">
                 <h3 class="text-xl md:text-2xl mb-1 font-bold">
                     <?php echo get_the_title(); ?>
@@ -70,69 +77,75 @@ if (is_home()) {
                         Read More
                     </button>
                 </a>
-                <?php endwhile;
-                wp_reset_query(); ?>
-
             </div>
+            <!-- End Info / Link Side of card -->
+
+            <!-- End Loop -->
+            <?php endwhile;
+            wp_reset_query(); ?>
         </div>
+        <!-- End Featured Post -->
 
+        <!-- Start All Other Posts -->
+        <div class="md:w-3/4 xl:w-7/12 mx-auto p-5">
+            <div class="grid grid-cols-12 gap-4 mt-6">
 
-    <div class="m-4 md:m-10 lg:max-w-4xl lg:mx-auto">
-        <!-- All Other Posts -->
-        <div class="grid grid-cols-12 gap-4 mt-6 ">
-            <div class="col-span-12 text-center mx-auto">
-                <h3 class="text2xl md:text-3xl mb-3 font-bold"><?php the_field('body_title_2', $post_id); ?></h3>
-            </div>
-            <?php
-            /*
-             * This little php block handles a few things.
-             * Overrides Wordpress' posts per page in admin.
-             * Setting this here allows for you to have it different somewhere else.
-             * Skip the first post since it is shown above in the featured post
-             * The if/else statement fixes pagination so it doesn't skip those too.
-             * Credit for the fix: https://wordpress.stackexchange.com/questions/261405/wp-query-with-offset-breaks-wp-pagenavi-or-any-pagination/335115#335115?newreg=0217492f5a8a47e28bf0c8a72200ebb9
-             *
-             */
-            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-            $per_page = 2; // How many posts do you want per page?
-            $default_offset = 1; // How much offset do you want?
-
-            if ($paged == 1) {
-                $offset = $default_offset;
-            } else {
-                $offset = (($paged - 1) * $per_page) + $default_offset;
-            }
-
-            $args = array(
-                'post_type' => 'post',
-                'posts_per_page' => $per_page,
-                'order' => 'DESC',
-                'offset' => $offset,
-                'paged' => $paged
-            );
-            $loop = new WP_Query($args);
-
-            // Start loop for all posts.
-            while ($loop->have_posts()) :
-                $loop->the_post();
-                ?>
-
-                <div class="col-span-12 p-5 mb-8 bg-gray-light shadow-lg rounded-lg">
-                    <h3 class="text-xl md:text-2xl font-bold"><?php echo get_the_title(); ?></h3>
-                    <p class="font-bold pb-3"><?php echo get_the_date(); ?> | <?php the_author(); ?></p>
-                    <p><?php echo the_excerpt(); ?></p>
-                    <a href="<?php the_permalink(); ?>">
-                        <button class="uppercase inline-block rounded-md mt-3 py-3 px-6 text-white bg-gray-dark hover:bg-gray-darkest transition duration-300">
-                            Read More
-                        </button>
-                    </a>
+                <!-- "All sermons" text -->
+                <div class="col-span-12 text-center mx-auto">
+                    <h3 class="text-2xl md:text-3xl mb-3 font-bold"><?php the_field('body_title_2', $post_id); ?></h3>
                 </div>
+                <!-- End "All sermons" text -->
 
-            <?php endwhile; ?>
+                <?php
+                /*
+                 * This little php block handles a few things.
+                 * Overrides Wordpress' posts per page in admin.
+                 * Setting this here allows for you to have it different somewhere else.
+                 * Skip the first post since it is shown above in the featured post
+                 * The if/else statement fixes pagination so it doesn't skip those too.
+                 * Credit for the fix: https://wordpress.stackexchange.com/questions/261405/wp-query-with-offset-breaks-wp-pagenavi-or-any-pagination/335115#335115?newreg=0217492f5a8a47e28bf0c8a72200ebb9
+                 *
+                 */
+                $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+                $per_page = 2; // How many posts do you want per page?
+                $default_offset = 1; // How much offset do you want?
+
+                if ($paged == 1) {
+                    $offset = $default_offset;
+                } else {
+                    $offset = (($paged - 1) * $per_page) + $default_offset;
+                }
+
+                $args = array(
+                    'post_type' => 'post',
+                    'posts_per_page' => $per_page,
+                    'order' => 'DESC',
+                    'offset' => $offset,
+                    'paged' => $paged
+                );
+                $loop = new WP_Query($args);
+
+                // Start loop for all posts.
+                while ($loop->have_posts()) :
+                    $loop->the_post();
+                    ?>
+
+                    <div class="col-span-12 p-5 mb-8 bg-gray-light shadow-lg rounded-lg">
+                        <h3 class="text-xl md:text-2xl font-bold"><?php echo get_the_title(); ?></h3>
+                        <p class="font-bold pb-3"><?php echo get_the_date(); ?> | <?php the_author(); ?></p>
+                        <p><?php echo the_excerpt(); ?></p>
+                        <a href="<?php the_permalink(); ?>">
+                            <button class="uppercase inline-block rounded-md mt-3 py-3 px-6 text-white bg-gray-dark hover:bg-gray-darkest transition duration-300">
+                                Read More
+                            </button>
+                        </a>
+                    </div>
+
+                <?php endwhile; ?>
+            </div>
+            <?php wpbeginner_numeric_posts_nav(); ?>
+
         </div>
-        <?php wpbeginner_numeric_posts_nav(); ?>
-
-    </div>
     </div>
 
 <?php
